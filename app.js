23169aa0fc8e0c5c0dbdb55e67b60d9f54adf741
gfournjibu <swahilisafarifame@gmail.com>
2017-12-05 17:42:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ var leChallenge = require('le-challenge-fs').create({
 
 le = LE.create({
     agreeToTerms: leAgree // hook to allow user to view and accept LE TOS
-        //,server: LE.productionServerUrl // or LE.productionServerUrl
-        , server: LE.stagingServerUrl 
+        ,server: LE.productionServerUrl // or LE.productionServerUrl
+        //, server: LE.stagingServerUrl 
         //, store: leStore 
 
         ,
